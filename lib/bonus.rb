24241813a67db8eb05374cf3b933b73c27884332
Jack Madden <jck13mad@gmail.com>
2020-02-02 15:29:38 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,7 +1,7 @@
 def bonus
  ##epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
- epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:capulet][:heroine][:status] = "dead"
+ epic_tragedy[:montague][:hero][:status] => "dead"
+  epic_tragedy[:capulet][:heroine][:status] => "dead"
 puts epic_tragedy
  
 #  =>
